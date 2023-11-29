75b2087cd8a84056e70f3a7b5dafe4e43679f508
lucasb13 <lucaseduardo131922@gmail.com>
2023-11-28 23:08:39 -0300

    docs update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const menuContent = document.querySelector('.content');
 const menuToggle = menuContent.querySelector('.menu-toggle');
 
 menuToggle.addEventListener('click', () => {
-    document.body.style.overflow = show ? 'hidden' : 'initial'
 
     menuContent.classList.toggle('on', show);
     show = !show;
